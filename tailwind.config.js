3221d1ec4f13b30558241ea5d9167a6e168b5931
fajarikas <fajarika.2021@student.uny.ac.id>
2023-06-27 05:57:41 +0700

    new slicing

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-new": "#4E4376",
+        "blue-soft": "#2B5876",
+        "ijo" : "#51BF99"
+      },
+    },
+    plugins: [],
+  },
+};
